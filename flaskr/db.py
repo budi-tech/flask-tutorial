9f97886e4836ed6a84cc652c8e531ef9031eef01
budi-tech <aboudeh.link@gmail.com>
2023-09-06 13:11:40 +0000

    added the python functions to the sql commands in the schema.sql file

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-import click  # noqa: F401
+import click
 from flask import current_app, g
 
 
@@ -19,3 +19,17 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource("schema.sql") as f:
+        db.executescript(f.read().decode("utf8"))
+
+
+@click.command("init-db")
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo("Initialized the database.")
