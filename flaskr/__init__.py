85f8dd8028c032423b2a30024e89a1081c1bd9b2
budi-tech <aboudeh.link@gmail.com>
2023-09-06 10:47:53 +0000

    correcting dunder init.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 
 
-def create_application(test_config=None):
+def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -24,6 +24,7 @@ def create_application(test_config=None):
     except OSError:
         pass
 
+    # a simple page that says hello
     @app.route("/hello")
     def hello():
         return "Hello, World!"
