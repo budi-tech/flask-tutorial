504718a8dccfe77ffc923046218c13ae7d3f50fe
budi-tech <aboudeh.link@gmail.com>
2023-09-07 12:21:04 +0000

    added load_logged_in_user function to load before any view function and made the value entered into the sql command in the login view a tupple by adding an apostrophe

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -55,7 +55,7 @@ def login():
         db = get_db()
         error = None
         user = db.execute(
-            "SELECT * FROM user WHERE username = ?", (username)
+            "SELECT * FROM user WHERE username = ?", (username,)
         ).fetchone()
 
         if user is None:
@@ -71,3 +71,15 @@ def login():
         flash(error)
 
     return render_template("auth/login.html")
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = (
+            get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+        )
