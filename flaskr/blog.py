4ac9ff42f07ca314489bc142d3b97efbc9f81071
budi-tech <aboudeh.link@gmail.com>
2023-09-08 11:46:11 +0000

    added the index route in the blog blueprint and added the index html jinja template

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -13,3 +13,14 @@ from flaskr.auth import login_required  # noqa: F401
 from flaskr.db import get_db  # noqa: F401
 
 bp = Blueprint("blog", __name__)
+
+
+@bp.route("/")
+def index():
+    db = get_db()
+    posts = db.execute(
+        "SELECT p.id, title, body, created, author_id, username"
+        " FROM post p JOIN user u ON p.author_id = u.id"
+        " ORDER BY created DESC"
+    ).fetchall()
+    return render_template("blog/index.html", posts=posts)
